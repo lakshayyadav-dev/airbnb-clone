e75fcc02334b9e301b2029e021893ccbe837c5ae
Lakshay <lakshayyadav2006@gmail.com>
2023-03-03 20:36:13 +0530

    Set up the project.

---
+++ client/postcss.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {config: './tailwind.config.cjs'}, // or name of your tailwind config file
+    autoprefixer: {},
+  },
+}
---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="bg-red-500">hello</div>
+    );
+}
+
+export default App;
